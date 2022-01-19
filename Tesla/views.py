ca16e50d9085fd185118a25fdac3456fd7334f7b
Dallon <dboultbee@hotmail.com>
2022-01-18 17:20:18 -0800

    primarily added chromedriver manager to pricechecks.py, fixed link redirect.

--- Tesla/views.py
+++ Tesla/views.py
@@ -5,7 +5,7 @@ import json
 
 
 def HomePage(request):
-    with open('C:/Users/Compuester/PycharmProjects/selenium/modelSCurrentPrices.json', 'r') as f:
+    with open('/home/bitnami/htdocs/projects/TeslaSpectra/selenium/modelSCurrentPrices.json', 'r') as f:
         #json.load provides us with a python dictionary from the json file
         prices = json.load(f)
 #Render the context as 'context_data' taking in the python dictionary as the key.
@@ -21,11 +21,11 @@ class ThanksPage(TemplateView):
 
 
 def modelSPrices(request):
-    with open('https://www.pythonanywhere.com/user/compuester/files/home/compuester/modelSCurrentPrices.json', 'r') as f:
+    with open('/home/bitnami/htdocs/projects/TeslaSpectra/selenium/modelSCurrentPrices.json', 'r') as f:
         #json.load provides us with a python dictionary from the json file
         prices = json.load(f)
 
-    with open('C:/Users/Compuester/PycharmProjects/selenium/modelSHistoricalPrices.json', 'r') as f2:
+    with open('/home/bitnami/htdocs/projects/TeslaSpectra/selenium/modelSHistoricalPrices.json', 'r') as f2:
         pastPrices = json.load(f2)
 #Render the context as 'context_data' taking in the python dictionaries as the key.
         return render(request, 'test.html',{'present_data': prices, 'past_data': pastPrices})
