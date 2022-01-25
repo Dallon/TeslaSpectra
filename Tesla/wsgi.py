701e8afe84ef63f3c237b6a37db319758011ac93
Dallon <dboultbee@hotmail.com>
2022-01-25 21:00:25 +0000

    changed the wsgi from teslaspectra.settings to Tesla.settings

--- Tesla/wsgi.py
+++ Tesla/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'teslaspectra.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Tesla.settings')
 
 application = get_wsgi_application()
