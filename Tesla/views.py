ccbfed1058f7e40c2cb241b60ce9188285482782
Dallon <dboultbee@hotmail.com>
2022-01-29 15:45:38 -0800

    changed directory of json files

--- Tesla/views.py
+++ Tesla/views.py
@@ -4,15 +4,15 @@ import json
 
 
 def HomePage(request):
-    with open('selenium/modelSCurrentPrices.json', 'r') as f:
+    with open('selenium/json/modelSCurrentPrices.json', 'r') as f:
         #json.load provides us with a python dictionary from the json file
         prices = json.load(f)
 #Render the context as 'context_data' taking in the python dictionary as the key.
 
-    with open('selenium/newpatent.json', 'r') as patentdetails:
+    with open('selenium/json/newpatent.json', 'r') as patentdetails:
         newestPatentDetails = json.load(patentdetails)
 
-    with open('selenium/rTeslaMotors.json', 'r') as redditpost:
+    with open('selenium/json/rTeslaMotors.json', 'r') as redditpost:
         teslareddit = json.load(redditpost)
 
         return render(request, 'index.html', {'present_data': prices, 'recentPatent': newestPatentDetails,
@@ -28,11 +28,11 @@ class ThanksPage(TemplateView):
 
 
 def modelSPrices(request):
-    with open('selenium/modelSCurrentPrices.json', 'r') as f:
+    with open('selenium/json/modelSCurrentPrices.json', 'r') as f:
         #json.load provides us with a python dictionary from the json file
         prices = json.load(f)
 
-    with open('selenium/modelSHistoricalPrices.json', 'r') as f2:
+    with open('selenium/json/modelSHistoricalPrices.json', 'r') as f2:
         pastPrices = json.load(f2)
 #Render the context as 'context_data' taking in the python dictionaries as the key.
         return render(request, 'test.html', {'present_data': prices, 'past_data': pastPrices})
