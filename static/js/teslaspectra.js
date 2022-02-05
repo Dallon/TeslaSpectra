09e0431b9ea3490c6d68c86fc68e151675e7cce7
Dallon <dboultbee@hotmail.com>
2022-02-04 21:56:39 -0800

    Ceate TeslaSpectra.js
    Add in JS for changing $ appearance across site

---
+++ static/js/teslaspectra.js
@@ -0,0 +1,7 @@
+$('h1').on('click', function(){
+    $(this).toggleClass('turnBlue')
+})
+
+$(document.documentElement).html(function(i,val){
+    return val.replace(/\$/g,"<span class ='dollasign'>$</span>");
+});
