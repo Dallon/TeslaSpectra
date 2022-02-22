4710abb1879e69dd76322baa38df045082da0980
Dallon <dboultbee@hotmail.com>
2022-02-21 21:12:02 -0800

    Imported Boto3,  changed exception handling, connections to json files in bucket, added hidden settings
    Starting at the top with this one we at boto3 and AWS hidden settings from settings file, allowing us to connect to our json files in the AWS bucket S3 storage. lines 15-30
    
    Next we add in the  code to write to the json files in bucket, as well as minor text changes indicating patent change. lines 69/70
    
    Fixed a link issue not specifying to click the anchor of the link "/a" line 77
    Finally we had our exceptions not printing out the exception which was silly. Fixed that, also Log the exception now.

--- selenium/Patent_App_check.py
+++ selenium/Patent_App_check.py
@@ -5,30 +5,32 @@ from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 from selenium.webdriver.firefox.options import Options
 
-
 options = Options()
 options.headless = True
-options.binary = FirefoxBinary(r'/usr/bin/iceweasel')
+# options.binary = FirefoxBinary(r'/usr/bin/iceweasel')
 driver = webdriver.Firefox(executable_path=GeckoDriverManager().install(), options=options)
 import os, json
 from datetime import datetime
 import logging
-"""Convert relative to absolute paths to avoid conflict in crontab"""
+import boto3
+from Tesla import settings
+
+#Convert relative to absolute paths to avoid conflict in crontab
 script_path = os.path.abspath(__file__) # i.e. /path/to/selenium/script.py
 script_dir = os.path.split(script_path)[0] #i.e. /path/to/selenium/
 
 logging.basicConfig(format="%(name)s - %(levelname)s - %(message)s",
                     filename=script_dir + '/logs/Patent_App_check.log', level=logging.INFO)
+
 dateTimeObj = datetime.now()
 
-
-
+# create  a connection to S3 using boto3 and the AWS access keys hidden in settings.py
+s3 = boto3.client('s3', aws_access_key_id=settings.aws_access_key_id,
+                  aws_secret_access_key=settings.aws_secret_access_key)
 
 """ uses Selenium to go to the US Patent webpage search engine and scrape the
 title of the most recent patent application name
 """
-
-
 def checkPatent():
     website = 'https://appft.uspto.gov/netacgi/nph-Parser?Sect1=PTO2&Sect2=HITOFF&p=1&u=%2Fnetahtml%2FPTO%2Fsearch-bool.html&r=0&f=S&l=50&TERM1=Tesla%2C+Inc&FIELD1=&co1=AND&TERM2=&FIELD2=&d=PG01'
 
@@ -46,6 +48,10 @@ def checkPatent():
     if os.stat(referencePatentName).st_size == 0:
         with open(script_dir + '/json/storedpatentname.json', 'w') as outfile:
             json.dump(patentTitle, outfile)
+
+        with open(script_dir + "/json/storedpatentname.json", "rb") as f:
+            s3.upload_fileobj(f, "teslaspectrajson", "storedpatentname.json")
+
             logging.info("storepatentname file was empty, populating with latest scrape")
 
     else:
@@ -60,15 +66,15 @@ def checkPatent():
             logging.info("There has been no new patent filing")
 
         else:
-            print("there has been a change to the webpage!")
-            logging.info("there has been a change to the webpage!")
+            print("there has been a new patent filing!!")
+            logging.info("there has been a new patent filing!")
             #patent check will do all the work for us including appending to the newpatent.json file
             try:
                 driver.get(website)
                 top_patent_title = driver.find_element(By.XPATH, '//tr[2]/td[3]').text
                 print("Tesla's most recent patent filing: {}".format(top_patent_title))
 
-                link = driver.find_element(By.XPATH, '//tr[2]/td[3]')
+                link = driver.find_element(By.XPATH, '//tr[2]/td[3]/a')
                 link.click()
                 date_filed = driver.find_element(By.XPATH, '//table/tbody/tr[3]/td[2]/b').text
                 abstract = driver.find_element(By.XPATH, '//body[@bgcolor="#FFFFFF"]/p[2]').text
@@ -83,12 +89,18 @@ def checkPatent():
                 with open(script_dir + '/json/newpatent.json', 'w') as outfile:
                     json.dump(patentEntry, outfile)
 
+                with open(script_dir + "/json/newpatent.json", "rb") as f:
+                    s3.upload_fileobj(f, "teslaspectrajson", "newpatent.json")
+
                 with open(script_dir + '/json/storedpatentname.json', 'w') as outfile:
                     json.dump(patentTitle, outfile)
 
+                with open(script_dir + "/json/storedpatentname.json", "rb") as f:
+                    s3.upload_fileobj(f, "teslaspectrajson", "storedpatentname.json")
             except Exception as e:
-                print("error occured")
-                logging.exception("an error occurred!")
+                print(e)
+                logging.exception(e)
+
             finally:
                 driver.close()
 
