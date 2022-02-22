8719b2c2cdf7d90d4444dd0a5dd2e8c774ea5cde
Dallon <dboultbee@hotmail.com>
2022-02-21 21:15:51 -0800

    importing Boto3/AWS Bucket access to views
    here we show addition of writing to local JSON files with the information stored in the AWS bucket json files. I have a feeling there is a more appropriate way to write this code, but for now its functional

--- Tesla/views.py
+++ Tesla/views.py
@@ -1,17 +1,29 @@
 from django.views.generic import TemplateView
 from django.shortcuts import render
 import json
+import boto3
+from Tesla import settings
+
+#create  a connection to S3 using boto3 and the AWS access keys hidden in settings.py
+s3 = boto3.client('s3', aws_access_key_id = settings.aws_access_key_id,
+                  aws_secret_access_key= settings.aws_secret_access_key)
 
 
 def HomePage(request):
+    with open("selenium/json/modelSCurrentPrices.json", "wb") as f:
+        s3.download_fileobj("teslaspectrajson", "modelSCurrentPrices.json", f)
     with open('selenium/json/modelSCurrentPrices.json', 'r') as f:
         #json.load provides us with a python dictionary from the json file
         prices = json.load(f)
-#Render the context as 'context_data' taking in the python dictionary as the key.
 
+        #Render the context as 'context_data' taking in the python dictionary as the key.
+    with open("selenium/json/newpatent.json", "wb") as f:
+        s3.download_fileobj("teslaspectrajson", "newpatent.json", f)
     with open('selenium/json/newpatent.json', 'r') as patentdetails:
         newestPatentDetails = json.load(patentdetails)
 
+    with open("selenium/json/newpatent.json", "wb") as f:
+        s3.download_fileobj("teslaspectrajson", "newpatent.json", f)
     with open('selenium/json/rTeslaMotors.json', 'r') as redditpost:
         teslareddit = json.load(redditpost)
 
@@ -28,10 +40,14 @@ class ThanksPage(TemplateView):
 
 
 def modelSPrices(request):
+    with open("selenium/json/modelSCurrentPrices.json", "wb") as f:
+        s3.download_fileobj("teslaspectrajson", "modelSCurrentPrices.json", f)
     with open('selenium/json/modelSCurrentPrices.json', 'r') as f:
         #json.load provides us with a python dictionary from the json file
         prices = json.load(f)
 
+    with open("selenium/json/modelSHistoricalPrices.json", "wb") as f:
+        s3.download_fileobj("teslaspectrajson", "modelSHistoricalPrices.json", f)
     with open('selenium/json/modelSHistoricalPrices.json', 'r') as f2:
         pastPrices = json.load(f2)
 #Render the context as 'context_data' taking in the python dictionaries as the key.
