efb11cc47995a4a8e0dbd614a14b14d1cf0ef3d8
Dallon <dboultbee@hotmail.com>
2022-02-21 21:17:41 -0800

    ignored changes

--- selenium/test.py
+++ selenium/test.py
@@ -1,6 +1,29 @@
-import os
-script_path = os.path.abspath(__file__) # i.e. /path/to/selenium/foobar.py
-script_dir = os.path.split(script_path)[0] #i.e. /path/to/selenium/
-print(script_dir)
-var = '/json/storedpatentname.json'
-print(script_dir + var)
+# import json
+# import boto3
+#
+# s3 = boto3.Session('s3')
+# json_object = 'test_file.json'
+# s3.put_object(
+#      Body=json.dumps(json_object),
+#      Bucket='teslaspectrajson',
+#      Key='test_file.json'
+# )
+import boto3
+from Tesla import settings
+
+
+#create  a connection to S3 using boto3 and the AWS access keys hidden in settings.py
+s3 = boto3.client('s3', aws_access_key_id = settings.aws_access_key_id,
+                  aws_secret_access_key= settings.aws_secret_access_key)
+
+#
+# #download the contents of the bucket-stored json file to the local json file.
+# with open("test_file.json", "wb") as f:
+#     s3.download_fileobj("teslaspectrajson", "test_file.json", f)
+
+
+#we have to open the local JSON file as a binary file before we can upload its contents
+# to the bucket stored file
+with open("test_file.json", "wb") as f:
+    s3.download_fileobj("teslaspectrajson", "test_file.json", f)
+
