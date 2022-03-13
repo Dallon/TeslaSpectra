225ca898049e62a9e5dccc149f687d790b2ad42c
Dallon <dboultbee@hotmail.com>
2022-03-12 17:43:01 -0800

    Update teslaspectra.js

--- static/js/teslaspectra.js
+++ static/js/teslaspectra.js
@@ -1,6 +1,5 @@
 $('h1').on('click', function(){
-    $(this).toggleClass('turnBlue')
-})
+    $(this).toggleClass('turnBlue')})
 
 $(document.documentElement).html(function(i,val){
     return val.replace(/\$/g,"<span class ='dollasign'>$</span>");
