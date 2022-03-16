dcd3b9969f8c94d040ce76ddcd94923ab280de5e
Dallon <dboultbee@hotmail.com>
2022-03-16 08:01:34 -0700

    driver.close() goes to driver.quit(), removed extra logging and print statements
    removed duplicate logging now handled by crontab automatically
    re ordered to be within PEP guidelines
    driver.close() swapped for driver.quit() to fix assumed caching issue.
    print statements commented out for better performance in "production"

--- selenium/pricechecks.py
+++ selenium/pricechecks.py
@@ -2,21 +2,16 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from datetime import datetime
-import time
 import os
 import json
-import logging
-"""Convert relative to absolute paths to avoid conflict in crontab"""
-script_path = os.path.abspath(__file__) # i.e. /path/to/selenium/script.py
-script_dir = os.path.split(script_path)[0] #i.e. /path/to/selenium/
-
-logging.basicConfig(format="%(name)s - %(levelname)s - %(message)s",
-                    filename=script_dir + '/logs/pricechecks.log', level=logging.INFO)
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 import boto3
 from logs import secretkeys
 
+"""Convert relative to absolute paths to avoid conflict in crontab"""
+script_path = os.path.abspath(__file__) # i.e. /path/to/selenium/script.py
+script_dir = os.path.split(script_path)[0] #i.e. /path/to/selenium/
 
 chrome_options = Options()
 chrome_options.add_argument("--headless")
@@ -33,12 +28,11 @@ date_format = '%Y-%m-%d %H:%M:%S.%f'
 s3 = boto3.client('s3', aws_access_key_id=secretkeys.aws_access_key_id,
                   aws_secret_access_key=secretkeys.aws_secret_access_key)
 
+
 def price_update():
     driver = webdriver.Chrome(options=chrome_options)
     try:
         driver.get(website)
-        time.sleep(3)
-        logging.info("process started-------------------------------------------------------------")
         ModelS = driver.find_element(By.XPATH, "//div[@id='root']//label[contains(@for, '$MTS12-Model')]//"
                                                "p[contains(@class, 'price-not-included')]").text
         ModelSPlaid = driver.find_element(By.XPATH, "//div[@id='root']//label[contains(@for, '$MTS11-Model')]//"
@@ -52,14 +46,12 @@ def price_update():
         prices = {'Model_S': ModelS,
                   'Model_S_Plaid': ModelSPlaid,
                   'scraped_at': str(dateTimeObj)}
-        print("the scraped prices are {}".format(prices))
-        logging.info("the scraped prices are {}".format(prices))
+        # print("the scraped prices are {}".format(prices))
 
         # Below we first check if the file is empty
         filepath = script_dir + '/json/modelSHistoricalPrices.json'
         if os.stat(filepath).st_size == 0:
             data = [prices]
-            logging.warning("had to populate the file with new data")
 
             with open(script_dir + '/json/modelSHistoricalPrices.json', 'w') as outfile:
                 json.dump(data, outfile)
@@ -70,8 +62,7 @@ def price_update():
         #  we don't do anything with the scraped data. If the values are different
         #  we append the scraped data to the assigned variable and overwrite the json file contents.
         else:
-            print("checking data in the historical prices file.... Loading file")
-            logging.info("checking data in the historical prices file.... Loading file")
+            # print("checking data in the historical prices file.... Loading file")
 
             with open(script_dir + "/json/modelSHistoricalPrices.json", "r") as read_content:
                 # here we convert the json object in the file into a python dict. These dictionaries
@@ -88,36 +79,32 @@ def price_update():
             histPriceModelSPlaid = sorted_dicts[-1]['Model_S_Plaid']
 
             if ModelS == histPriceModelS and ModelSPlaid == histPriceModelSPlaid:
-                print("prices haven't changed")
-                logging.info("prices haven't changed")
+                # print("prices haven't changed")
                 pass
             else:
-                print("prices have changed--- logging changes")
+                # print("prices have changed--- logging changes")
                 with open(script_dir + "/json/modelSCurrentPrices.json", "w") as outfile:
                     json.dump(prices, outfile)
                     print("current price json updated")
-                    logging.info("current price json updated")
 
                 with open(script_dir + '/json/modelSCurrentPrices.json', 'rb') as f:
                     s3.upload_fileobj(f, "teslaspectrajson", "modelSCurrentPrices.json")
 
                 sorted_dicts.append(prices)
-                print(sorted_dicts)
+                # print(sorted_dicts)
                 with open(script_dir + "/json/modelSHistoricalPrices.json", "w") as outfile:
                     json.dump(sorted_dicts, outfile)
 
                 with open(script_dir + '/json/modelSHistoricalPrices.json', 'rb') as f:
                     s3.upload_fileobj(f, "teslaspectrajson", "modelSHistoricalPrices.json")
-                    print("historical prices json updated")
-                    logging.info("historical prices json updated")
+                    # print("historical prices json updated")
 
     except Exception as e:
         print(e)
-        logging.exception(e)
 
     finally:
-        driver.close()  # refers to the connection to the selenium driver
-        logging.info("program completed-------------------------------------------------------")
+        driver.quit()  # refers to the connection to the selenium driver
+        print("driver.quit")
 
 
 price_update()
