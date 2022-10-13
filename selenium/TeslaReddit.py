fe5143d747834656f1eccf7d7033c771e58a80d6
Dallon <dboultbee@hotmail.com>
2022-10-12 19:58:34 -0700

    moved Try: clause, limited range of runtime, fixed broken xpath to postlink
    The Try: clause should start at the beginning of the  function so that the catch exceptions line is run, ensuring driver.quit() occurs.
    
    The while True: loop has the potential to run continuously if "hours" is not found, so we need to make sure there is a limit to how many posts are checked for the word "hours", which is done with for postNumber in range(0, 10);
    
    The last part to fix was a broken xpath for postLink.

--- selenium/TeslaReddit.py
+++ selenium/TeslaReddit.py
@@ -31,99 +31,107 @@ driver = webdriver.Firefox(service=s, options=options)
 
 
 def rTeslaMotors():
-    website = 'http://www.reddit.com/r/teslamotors'
-    driver.get(website)
-    postTimeStamp = driver.find_element(By.XPATH, "//span[@data-testid = 'post_timestamp']").text
-    splitString = postTimeStamp.split()
-    postTimeStamp = splitString
-    liveEvent = "//div[contains(@class, 'rpBJOH')]//div[@data-testid='post-container']//following-sibling::span[text()='Live']"
-    # print("Confirm scaper works by taking first listed post's timestamp and displaying it:" + str(splitString))
+    try:
+        website = 'http://www.reddit.com/r/teslamotors'
+        driver.get(website)
+        postTimeStamp = driver.find_element(By.XPATH, "//span[@data-testid = 'post_timestamp']").text
+        splitString = postTimeStamp.split()
+        postTimeStamp = splitString
+        liveEvent = "//div[contains(@class, 'rpBJOH')]//div[@data-testid='post-container']//following-sibling::span[text()='Live']"
+        # print("Confirm scaper works by taking first listed post's timestamp and displaying it:" + str(splitString))
 
-    """The while loop below checks for the presence of 'hours' in the timestamp of the 1st post, 
-    re-assigning the variable firstPostTimeStamp to the next timestamp xpath if 'hours' is not found. if hours not found
-     it continues reassigning the variable until there is 'hours' in the timestamp. Then we know we've 
-    reached the most upvoted OR the first pinned post of the past 24 hours"""
-    postNumber = 0
-    while True:
-        if postTimeStamp[1] != 'hours':
-            postNumber = postNumber + 1
-            postTimeStamp = driver.find_element(By.XPATH, "(//span[@data-testid = 'post_timestamp'])[{}]".format(postNumber)).text
-            postTimeStamp = postTimeStamp.split()
-            # print("The #{} post's timestamp is:".format(postNumber) + str(postTimeStamp))
-        else:
-        # Because PostNumber represents the index number of the post we can use it to gather
-        # the relevant post data
+        """The while loop below checks for the presence of 'hours' in the timestamp of the 1st post, 
+        re-assigning the variable firstPostTimeStamp to the next timestamp xpath if 'hours' is not found. if hours not found
+         it continues reassigning the variable until there is 'hours' in the timestamp. Then we know we've 
+        reached the most upvoted OR the first pinned post of the past 24 hours"""
+        postNumber = 0
+        for postNumber in range(0, 10):
+            # print(postTimeStamp[1])
+            if postTimeStamp[1] != "hours":
 
-            try:
-                firstPostTitle = driver.find_element(By.XPATH, "(//div[contains(@class, 'rpBJOH')]//"
-                 "div[@data-testid='post-container']//h3)[{}]".format(postNumber)).text
-                # print("the #{} post timestamp contains {}"
-    # " making it the first hot post of the day".format(postNumber,postTimeStamp[1]))
-
-                print("The first hot post Title is: {}".format(firstPostTitle))
-                Upvotes = driver.find_element(By.XPATH, "(//div[contains(@class, 'rpBJOH')]//"
-                 "div[@data-testid='post-container'])[{}]//div[contains(@id, 'vote-arrows')]".format(postNumber)).text
-                print("The upvotes were sucessfully scraped at: {}".format(Upvotes))
-
-                ## This section essentially checks if there is a live event. If there is a live event
-                ## then the script tries to find the link and fails because live event links aren't
-                ##in the same format as standard hot post links
-                postlink = driver.find_elements(By.XPATH, "(//div[contains(@class, 'rpBJOH')]"
-"//div[@data-testid='post-container'])[{}]//a[@data-click-id='body']".format(postNumber))
-                if postlink == True:
-                    postlink = driver.find_element(By.XPATH, "(//div[contains(@class, 'rpBJOH')]//div[@data-testid='post-container'])[{}]//a[@data-click-id='body']".format(postNumber)).get_attribute("href")
-                else:
-                    postlink = driver.find_element(By.XPATH, "//a[contains(@href, 'predictions')]").get_attribute("href")
+                postNumber = postNumber + 1
+                postTimeStamp = driver.find_element(By.XPATH, "(//span[@data-testid = 'post_timestamp'])[{}]".format(postNumber)).text
+                postTimeStamp = postTimeStamp.split()
+                # print("The #{} post's timestamp is:".format(postNumber) + str(postTimeStamp))
+            else:
+            # Because PostNumber represents the index number of the post we can use it to gather
+            # the relevant post data
 
 
+                    firstPostTitle = driver.find_element(By.XPATH, "(//div[contains(@class, 'rpBJOH')]//"
+                     "div[@data-testid='post-container']//h3)[{}]".format(postNumber)).text
+                    # print("the #{} post timestamp contains {}"
+        # " making it the first hot post of the day".format(postNumber,postTimeStamp[1]))
+
+                    # print("The first hot post Title is: {}".format(firstPostTitle))
+                    Upvotes = driver.find_element(By.XPATH, "(//div[contains(@class, 'rpBJOH')]//"
+                     "div[@data-testid='post-container'])[{}]//div[contains(@id, 'vote-arrows')]".format(postNumber)).text
+                    # print("The upvotes were sucessfully scraped at: {}".format(Upvotes))
+
+                    ## This section essentially checks if there is a live event. If there is a live event
+                    ## then the script tries to find the link and fails because live event links aren't
+                    ##in the same format as standard hot post links
+                    postlink = driver.find_elements(By.XPATH, "(//div[contains(@class, 'rpBJOH')]//div[@data-testid='post-container']//a[@data-click-id = 'body'])[{}]".format(postNumber))
+                    if postlink:
+                        postlink = driver.find_element(By.XPATH, "(//div[contains(@class, 'rpBJOH')]//div[@data-testid='post-container']//a[@data-click-id = 'body'])[{}]".format(postNumber)).get_attribute("href")
+                    else:
+                        postlink = driver.find_element(By.XPATH, "//a[contains(@href, 'predictions')]").get_attribute("href")
 
 
-                #the if statement below allows us to view the upvotes as a number.
-                if "." in Upvotes:
-                    Upvotes = Upvotes.replace("k", "00").replace(".", "")
-
-                #if there are no votes, the votes placeholder says Vote. we need to change that
-                #to avoid an error.
-                if Upvotes == 'Vote':
-                    Upvotes = 0
-
-                if 100 < int(Upvotes) <= 1000:
-                    storedUpvotes = "{}00+".format(str(Upvotes)[0])
-                    hotPost = ''
-                elif int(Upvotes) > 1000:
-                    hotPost = ''
-                    storedUpvotes = "{}{}00+".format(str(Upvotes)[0], str(Upvotes)[1])
-                elif int(Upvotes) > 1500:
-                    hotpost = 'Hot'
-                else:
-                    storedUpvotes = "{}0+".format(str(Upvotes)[0])
-                    hotPost = ''
-
-                newscraped = {'postTitle': firstPostTitle,
-                             'postUpvotes': storedUpvotes,
-                             'hotPost': hotPost,
-                             'scraped_at': str(dateTimeObj),
-                              "postLink":postlink}
 
 
-                with open(script_dir + '/json/rTeslaMotors.json', 'w') as outfile:
-                    json.dump(newscraped, outfile)
-                # print("r/TeslaMotors local JSON populated")
-                logging.info("r/TeslaMotors local JSON populated")
-                with open(script_dir + "/json/rTeslaMotors.json", "rb") as f:
-                    s3.upload_fileobj(f, "teslaspectrajson", "rTeslaMotors.json")
-                # print("r/TeslaMotors S3 JSON populated")
-                logging.info("r/TeslaMotors S3 JSON populated")
+                    #the if statement below allows us to view the upvotes as a number.
+                    if "." in Upvotes:
+                        Upvotes = Upvotes.replace("k", "00").replace(".", "")
 
-            except Exception as e:
-                print(e)
-                logging.exception(e)
+                    #if there are no votes, the votes placeholder says Vote. we need to change that
+                    #to avoid an error.
+                    if Upvotes == 'Vote':
+                        Upvotes = 0
 
-            finally:
-                driver.quit()
-                # print("driver quit")
-                logging.info("driver.quit")
-                break #due to while true loop if we don't break the program doesn't end
+                    if 100 < int(Upvotes) <= 1000:
+                        storedUpvotes = "{}00+".format(str(Upvotes)[0])
+                        hotPost = ''
+                    elif int(Upvotes) > 1000:
+                        hotPost = ''
+                        storedUpvotes = "{}{}00+".format(str(Upvotes)[0], str(Upvotes)[1])
+                    elif int(Upvotes) > 1500:
+                        hotpost = 'Hot'
+                    else:
+                        storedUpvotes = "{}0+".format(str(Upvotes)[0])
+                        hotPost = ''
+
+                    newscraped = {'postTitle': firstPostTitle,
+                                 'postUpvotes': storedUpvotes,
+                                 'hotPost': hotPost,
+                                 'scraped_at': str(dateTimeObj),
+                                  "postLink":postlink}
+
+                    with open(script_dir + '/json/rTeslaMotors.json', 'w') as outfile:
+                        json.dump(newscraped, outfile)
+                    # print("r/TeslaMotors local JSON populated")
+                    logging.info("r/TeslaMotors local JSON populated")
+                    with open(script_dir + "/json/rTeslaMotors.json", "rb") as f:
+                        s3.upload_fileobj(f, "teslaspectrajson", "rTeslaMotors.json")
+                    # print("r/TeslaMotors S3 JSON populated")
+                    logging.info("r/TeslaMotors S3 JSON populated")
+                    break
+
+    except Exception as e:
+        print(e)
+        driver.quit()
+
+        logging.exception(e)
+
+    finally:
+        driver.quit()
+        # print("driver quit")
+        logging.info("driver.quit")
+
+
+
+
+
 
 rTeslaMotors()
 
