5349c124fbeed94d096788441989170051ae1a63
Dallon <dboultbee@hotmail.com>
2023-01-06 15:10:41 -0800

    helper function for patent_app_check
    this script is a helper function which is used in patent_app_check to verify if we need to scrape a new patent from the search engine, or maybe multiple patents have been published that need to be stored.

---
+++ selenium/store_patent_info.py
@@ -0,0 +1,62 @@
+from selenium.webdriver.common.by import By
+import os
+import json
+import time
+import boto3
+from logs import secretkeys
+#Convert relative to absolute paths to avoid conflict in crontab
+script_path = os.path.abspath(__file__)  # i.e. /path/to/selenium/script.py
+script_dir = os.path.split(script_path)[0]  #i.e. /path/to/selenium/
+
+# create  a connection to S3 using boto3 and the AWS access keys hidden in settings.py
+s3 = boto3.client('s3', aws_access_key_id=secretkeys.aws_access_key_id,
+                  aws_secret_access_key=secretkeys.aws_secret_access_key)
+
+
+#this function assumes you have already populated the json file with a list of dictionaries with
+#the same format as exampleList
+def store_patent_info(driver, patentEntry):
+
+    with open(script_dir + '/json/scrapedpatents.json', 'r') as readfile:
+        archivedPatent = json.load(readfile)
+    for i in range(1, 5):
+        #in case there is more than one patent published in a day, iterate through the rows
+        # in column 14 in the table shown, comparing the previously scraped title stored in
+        # our json file with the newly scraped title
+        patentTitle = driver.find_element(By.XPATH, "(//div[@data-cell=14])[{}]".format(i)).text
+        # print(archivedPatent["patentTitle"])
+        # print(archivedPatent["patent{}".format(i)])
+        # Check if patentTitle is already in archivedPatent
+        already_scraped = False
+        for value in archivedPatent.values():
+            if value == patentTitle:
+                already_scraped = True
+                break
+
+        if already_scraped:
+            print("There has been no new patent published")
+            break
+
+        else:
+            print("there has been a new patent published!")
+            patentPublishDate = driver.find_element(By.XPATH, "(//div[@data-cell=11])[{}]".format(i)).text
+            print(patentPublishDate)
+            patentDocButton = driver.find_element(By.XPATH, "(//div[@data-cell=10])[{}]".format(i))
+            patentDocButton.click()
+            time.sleep(3)
+            #once the doc button is clicked we can scrape the abstract from the now visible patent document
+            patentAbstract = driver.find_element(By.XPATH, "//p[@data-section = "
+                                                           "'abstract' and @id = '3']").text
+            patentEntry["patent{}".format(i)] = patentTitle
+            patentEntry["publishing date{}".format(i)] = patentPublishDate
+            patentEntry["abstract{}".format(i)] = patentAbstract
+
+
+
+    with open(script_dir + '/json/scrapedpatents.json', 'w') as outfile:
+        json.dump(patentEntry, outfile)
+
+    with open(script_dir + "/json/scrapedpatents.json", "rb") as f:
+        s3.upload_fileobj(f, "teslaspectrajson", "scrapedpatents.json")
+        print("new patent details uploaded to S3")
+
