5e513636628e94bd1d57e11c6a98582396f58ab0
Dallon <dboultbee@hotmail.com>
2023-01-10 18:17:04 -0800

    Added custom filter to combine strings
    From Stackoverflow*, found a way to add strings so that my django filter-function in onyoutube.html is able to iterate through the loop properly.

--- teslaspectra/templatetags/custom_tags.py
+++ teslaspectra/templatetags/custom_tags.py
@@ -5,3 +5,9 @@ register = template.Library()
 @register.filter(name='get_item')
 def get_item(dictionary, key):
     return dictionary.get(key)
+
+@register.filter(name = 'addstr')
+def addstr(arg1, arg2):
+    """concatenate arg1 & arg2"""
+    new_string = str(arg1 + arg2)
+    return str(new_string)
