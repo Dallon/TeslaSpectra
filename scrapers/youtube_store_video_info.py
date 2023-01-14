cce82bcb0ad6ba9925ca3765c333a6d85f0260bf
Dallon <dboultbee@hotmail.com>
2023-01-13 17:31:04 -0800

    added if clause to avoid URL embed error if the video had already been scraped
    if the video had already been scraped it goes through this loop still, and as a result it would end up inserting "-nocookie" into the embed url twice, making the link invalid.

--- scrapers/youtube_store_video_info.py
+++ scrapers/youtube_store_video_info.py
@@ -3,7 +3,10 @@ def store_video_details(videoDetails, title, description, embed_url, published_a
     videoDetails["{} vid_title".format(k)] = title
     videoDetails["{} e_tag".format(k)] = e_tag
     videoDetails["{} description".format(k)] = description
-    videoDetails["{} embed_url".format(k)] = embed_url[0:19] + "-nocookie" + embed_url[19:]
+    if "-nocookie" not in embed_url:
+        videoDetails["{} embed_url".format(k)] = embed_url[0:19] + "-nocookie" + embed_url[19:]
+    else:
+        videoDetails["{} embed_url".format(k)]= embed_url
     videoDetails["{} published_at".format(k)] = published_at
     #the channel title
     videoDetails["{}".format(k)] = k
