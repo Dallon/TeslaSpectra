2869062d84b7243c56acbae30420fd4fe14da14c
Dallon <dboultbee@hotmail.com>
2023-02-07 16:39:36 -0800

    add base to the url patterns
    So that we can use the accepted_cookies variable in our base.html template

--- teslaspectra/urls.py
+++ teslaspectra/urls.py
@@ -21,6 +21,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.homepage, name='home'),
+    path('', views.base, name='base'),
     path('accounts/', include('accounts.urls',namespace='accounts')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('onyoutube/', views.youtube_page, name='onyoutube'),
