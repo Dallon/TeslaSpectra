1d9957414c3b9a48e8b9fcfe3affb91c29b9376c
Dallon <dboultbee@hotmail.com>
2023-02-23 18:00:31 -0800

    Changed default selector from "SAME" to "ADJ" in patent search
    "SAME" still has patents show up where Tesla and inc are anywhere in the document whereas ADJ shows up only if they are next to eachother- this change prevents unwanted patents by other companies being attributed to the scraped patents json file

--- scrapers/patent_app_check.py
+++ scrapers/patent_app_check.py
@@ -12,7 +12,7 @@ from patent_store_info import store_patent_info
 from datetime import datetime
 options = Options()
 options.headless = True
-# options.binary = FirefoxBinary(r'/usr/bin/iceweasel')
+options.binary = FirefoxBinary(r'/usr/bin/iceweasel')
 
 
 """ uses Selenium to go to the US Patent webpage search engine and scrape the
@@ -28,7 +28,7 @@ def check_patent():
     try:
         website = 'https://ppubs.uspto.gov/pubwebapp/'
         driver.get(website)
-        time.sleep(5)
+        time.sleep(10)
 
         # initialize the input search textbox
         inputText = driver.find_element(By.CLASS_NAME, "trix")
@@ -37,7 +37,7 @@ def check_patent():
         searchDefaultOperator = Select(driver.find_element(By.CLASS_NAME, "operator"))
         selectAllDBsButton.click()
         USPatDBSelect.click()
-        searchDefaultOperator.select_by_value("SAME")
+        searchDefaultOperator.select_by_value("ADJ")
         inputText.click()
         inputText.send_keys("Tesla, Inc"[::-1])
         searchButton = driver.find_element(By.ID, "search-btn-search")
