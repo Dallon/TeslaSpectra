aa9adfa402afb00bfb04854cf3a4a9e845996727
Dallon <dboultbee@hotmail.com>
2023-03-09 17:38:19 -0800

    Made the script more robust with try clauses, wait functions
    Added in try, wait, with exception clauses for certain parts of the scraper to make it more robust. Some patents dont have abstracts for example, and sometimes the page loading time varies past 30 seconds, these updates ensure 100 seconds of page load time both for when we click the search button and when we go to scrape the patent document provided later in the script. Also added exceptions for error handling.

--- scrapers/patent_store_info.py
+++ scrapers/patent_store_info.py
@@ -1,4 +1,7 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
 import os
 import json
 import time
@@ -27,23 +30,32 @@ def store_patent_info(driver, patentEntry):
     with open(script_dir + '/json/scrapedpatents.json', 'r') as readfile:
         archivedPatent = json.load(readfile)
     newPatentScraped = False
+    # Wait for the search results to load using WebDriverWait and check for presence of the 1st row of
+    #cell 14 to load.
+    try:
+        wait = WebDriverWait(driver, 100)
+        wait.until(EC.presence_of_element_located((By.XPATH, "(//div[@data-cell=14])[1]")))
+
+    except TimeoutException:
+        print("Search results not found within timeout period of 100 seconds")
     for i in range(1, 5):
+
         #in case there is more than one patent published in a day, iterate through the rows
         # in column 14 in the table shown, comparing the previously scraped title stored in
         # our json file with the newly scraped title
-        scraped_patent_title = driver.find_element(By.XPATH, "(//div[@data-cell=14])[{}]".format(i)).text
-        # print(archivedPatent["patent{}".format(i)])
 
+        scraped_patent_title = driver.find_element(By.XPATH, "(//div[@data-cell=14])[{}]".format(i)).text
+
+        # print(archivedPatent["patent{}".format(i)])
         # Check if scraped_patent_title just scraped is already in archivedPatent.
 
         already_scraped = False
 
         for value in archivedPatent.values():
-            # If it is, we don't need to look any further because the patents are in descending
-            # order of date published.
+            # If scraped_patent_title is already in archivedPatent, we don't need to look any further
+            # because the patents are in descending order of date published.
             if scraped_patent_title == value:
                 already_scraped = True
-
                 break
 
         if already_scraped:
@@ -59,10 +71,22 @@ def store_patent_info(driver, patentEntry):
             logging.info(patentPublishDate)
             patentDocButton = driver.find_element(By.XPATH, "(//div[@data-cell=10])[{}]".format(i))
             patentDocButton.click()
-            time.sleep(3)
-            #once the doc button is clicked we can scrape the abstract from the now visible patent document
-            patentAbstract = driver.find_element(By.XPATH, "//p[@data-section = "
-                                                           "'abstract' and @id = '3']").text
+            #wait for presence of loaded patent document
+            try:
+                wait = WebDriverWait(driver, 100)
+                wait.until(EC.presence_of_element_located((By.XPATH, "//div[@class= 'docMetadata']")))
+
+            except TimeoutException:
+                print("Patent document not found within timeout period of 100 seconds")
+            #once the doc button is clicked and the document is loaded
+            # we can scrape the abstract from the now visible patent document if its there, otherwise
+            #we will assign the text  value of "No Abstract" to the patentAbstract
+
+            try:
+                patentAbstract = driver.find_element(By.XPATH, "//p[@data-section='abstract' and @id='3']").text
+            except NoSuchElementException:
+                patentAbstract = "No Abstract"
+
             patentEntry["patent{}".format(i)] = scraped_patent_title
             patentEntry["publishing date{}".format(i)] = patentPublishDate
             patentEntry["abstract{}".format(i)] = patentAbstract
